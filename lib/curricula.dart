77b3f08de5d3da13dc65b750595b587da390b8d3
Németh Balázs <nemethb.business@gmail.com>
2024-07-07 18:36:52 +0200

    Added curriculum for bme vik mi

---
+++ lib/curricula.dart
@@ -0,0 +1,399 @@
+import 'package:kreditindex_calculator/subject.dart';
+
+int seqnum = 1;
+
+class Curricula {
+
+  static List<Subject> bme_vik_mi = [
+    Subject(
+        newId: -1,
+        newName: 'Analízis 1 informatikusoknak',
+        newWeight: 6,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 1,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Analízis 2 informatikusoknak',
+        newWeight: 6,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 2,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Valószínűségszámítás és statisztika',
+        newWeight: 6,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 3,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Bevezetés a számításelméletbe 1',
+        newWeight: 6,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 1,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Bevezetés a számításelméletbe 2',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 2,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Kódolástechnika',
+        newWeight: 4,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 3,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Algoritmuselmélet',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 2,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Fizikai alapismeretek',
+        newWeight: 2,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 1,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Fizika I',
+        newWeight: 4,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 2,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Mikro- és makroökonómia B',
+        newWeight: 3,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 6,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Menedzsment és vállalkozásgazdaságtan',
+        newWeight: 4,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 4,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Jogi alapismeretek',
+        newWeight: 3,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 4,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Digitális kompetenciák',
+        newWeight: 2,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 1,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Villamos alapismeretek',
+        newWeight: 3,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 1,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Digitális technika',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 1,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Számítógép-architektúrák',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 2,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Kommunikációs hálózatok',
+        newWeight: 7,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 4,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Operációs rendszerek',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 3,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'A programozás alapjai 1',
+        newWeight: 7,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 1,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'A programozás alapjai 2',
+        newWeight: 6,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 2,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'A programozás alapjai 3',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 3,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Adatbázisok',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 3,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Szoftvertechnológia',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 3,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Szoftvertechnikák',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 4,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Szoftver projekt laboratórium',
+        newWeight: 4,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 4,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Számítógépes grafika',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 4,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Mesterséges intelligencia',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 5,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'IT biztonság',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 6,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Kötelezően választható szakmai tantárgy 1',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 5,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Kötelezően választható szakmai tantárgy 2',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 5,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Kötelezően választható szakmai tantárgy 3',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 6,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Kötelezően választható szakmai tantárgy 4',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 6,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Kötelezően választható szakmai tantárgy 5',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 7,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Specializáció ágazati főtantárgy',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 5,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Specializáció ágazati laboratórium',
+        newWeight: 3,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 6,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Specializáció tantárgy 2',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 5,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Specializáció tantárgy 3',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 5,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Önálló laboratórium',
+        newWeight: 5,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 6,
+        newOptional: false
+    ),
+    Subject(
+        newId: -1,
+        newName: 'Szakdolgozat-készítés',
+        newWeight: 15,
+        newGrade: 5,
+        newSure: true,
+        newSeqnum: seqnum++,
+        newSemester: 7,
+        newOptional: false
+    ),
+  ];
+}
